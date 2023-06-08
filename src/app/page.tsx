202991fbbfe9ee10b1e9888c94a20d51f6718d8f
vchoo27 <37387062+vchoo27@users.noreply.github.com>
2023-06-08 16:21:19 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ import { Button } from "@/components/ui/button"
 export default function HomePage() {
   return (
     <main className="">
-      <h1> Hello world</h1>
+      <h1> Hello world bam!</h1>
       <h2 className="font-display">Display Font</h2>
-
-
-
     </main>
-
   )
 }
