025daa3d60f8fa282887b07ed893082f28b6f8d9
Blake Ingenthron <blakeingenthron@gmail.com>
2023-07-13 17:35:39 -0500

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link"
 
-import { SiteHeader } from "@/components/site-header"
-
 export default function NotFound() {
   return (
     <>
-      <SiteHeader />
       <main className="container mx-auto flex w-full flex-auto flex-col justify-center  py-24 sm:py-64 ">
         <p className="text-accent-600 text-base font-semibold leading-8">404</p>
         <h1 className="text-accent-900 mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
